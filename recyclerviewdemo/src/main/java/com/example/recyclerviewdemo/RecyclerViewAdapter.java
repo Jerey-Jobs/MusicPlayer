be2010bc663953d21420ff29c9e12c07a9a2ba28
Xiamin <610315802@qq.com>
2016-09-02 18:43:15 +0800

    增加RecyclerView Demo 后续的歌曲列表会使用RecyclerView替代ListView

---
+++ recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/RecyclerViewAdapter.java
@@ -0,0 +1,86 @@
+package com.example.recyclerviewdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Xiamin on 2016/9/2.
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private String[] mdata;
+
+    public RecyclerViewAdapter(String[] data){
+        mdata = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.textView.setText(mdata[position]);
+
+        /**
+         * 在绑定viewholder时 给每个view设置上触摸监听
+         */
+        if(mOnItemClickListener != null)
+        {
+            holder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mOnItemClickListener.onItemClick(holder.itemView ,position);
+                }
+            });
+
+            holder.textView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    mOnItemClickListener.onItemLongClick(holder.itemView ,position);
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.length;
+    }
+
+    /**
+     * 定义接口用于短按长按的回调
+     */
+    public interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+
+    /**
+     * 添加点击事件
+     */
+    private OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.textView);
+        }
+    }
+}
