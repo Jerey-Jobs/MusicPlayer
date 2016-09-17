b1d22f88fa25bdce95a757f174c45be207e4d11c
Xiamin <610315802@qq.com>
2016-09-17 11:57:07 +0800

    解决欢迎界面上面出现通知栏的问题

--- app/src/main/java/com/example/xiamin/musicplayer/Activity/WelcomeActivity.java
+++ app/src/main/java/com/example/xiamin/musicplayer/Activity/WelcomeActivity.java
@@ -6,8 +6,11 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.example.xiamin.musicplayer.R;
 
@@ -21,6 +24,19 @@ public class WelcomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         final View view = View.inflate(this, R.layout.welcome, null);
         setContentView(view);
+
+        /**
+         * 解决欢迎界面上面出现通知栏的问题，伪全屏
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // LOLLIPOP解决方案
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // KITKAT解决方案
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         /**
          * 改成放大的属性动画
          */
