3490338a91745676bd4adcf9ae378ef0d623c88a
Xiamin <610315802@qq.com>
2016-09-20 22:50:29 +0800

    在线音乐列表成功加载，解决图片加载时错乱的问题，且优先使用本地数据。 同时在两种view切换加载时，易出view因重复加载导致gettag时viewholder错误问题

--- app/src/main/java/com/example/xiamin/musicplayer/adapter/SongListAdapter.java
+++ app/src/main/java/com/example/xiamin/musicplayer/adapter/SongListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.xiamin.musicplayer.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,20 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.xiamin.musicplayer.Bean.OnlineMuiscBean;
 import com.example.xiamin.musicplayer.Bean.SongListInfo;
 import com.example.xiamin.musicplayer.R;
+import com.example.xiamin.musicplayer.utils.Constants;
+import com.example.xiamin.musicplayer.utils.JsonCallBack.JsonCallBack;
+import com.example.xiamin.musicplayer.utils.JsonCallBack.JsonOnlineMusicList;
+import com.zhy.http.okhttp.OkHttpUtils;
 
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by Xiamin on 2016/9/18.
@@ -24,8 +32,9 @@ public class SongListAdapter extends BaseAdapter {
     private Context mContext;
     private List<SongListInfo> mData;
 
-    public SongListAdapter(List<SongListInfo> data) {
+    public SongListAdapter(List<SongListInfo> data, Context context) {
         mData = data;
+        mContext = context;
     }
 
     @Override
@@ -43,20 +52,45 @@ public class SongListAdapter extends BaseAdapter {
         return i;
     }
 
+    /**
+     * 在两种view切换加载时，易出view因重复加载导致viewholder错误问题
+     * 暂时采用这种办法
+     * @param i
+     * @param view
+     * @param viewGroup
+     * @return
+     */
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         ViewHolderMusicList viewHolderMusicList;
+        ViewHolderProfile viewHolderProfile;
+        if (mData.get(i).getType().equals("#")) {
+            Log.w("iii", "mData.get(i).getType().equals(#)");
+//            if (view == null) {
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_holder_song_list_profile, viewGroup, false);
+            viewHolderProfile = new ViewHolderProfile(view);
+            view.setTag(viewHolderProfile);
+//            } else {
+//                viewHolderProfile = (ViewHolderProfile) view.getTag();
+//            }
+//
+            viewHolderProfile.tvProfile.setText(mData.get(i).getTitle());
 
-        if (view == null) {
+            //  return view;
+        } else {
+//            if (view == null) {
             view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_holder_song_list, viewGroup, false);
             viewHolderMusicList = new ViewHolderMusicList(view);
             view.setTag(viewHolderMusicList);
-        } else {
-            viewHolderMusicList = (ViewHolderMusicList) view.getTag();
+//            } else {
+//                viewHolderMusicList = (ViewHolderMusicList) view.getTag();
+//            }
+
+            viewHolderMusicList.vDivider.setVisibility(View.VISIBLE);
+            viewHolderMusicList.tvMusic1.setText(mData.get(i).getTitle());
+            setMusicListItemInfo(mData.get(i), viewHolderMusicList);
         }
 
-        viewHolderMusicList.vDivider.setVisibility(View.VISIBLE);
-        viewHolderMusicList.tvMusic1.setText(mData.get(i).getTitle());
 
         return view;
     }
@@ -77,4 +111,96 @@ public class SongListAdapter extends BaseAdapter {
             ButterKnife.bind(this, view);
         }
     }
+
+    class ViewHolderProfile {
+        @Bind(R.id.tv_profile)
+        TextView tvProfile;
+
+        public ViewHolderProfile(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+
+    /**
+     * 1.判断是否有数据，若没有 网络请求加载  有 直接加载
+     *
+     * @param songListInfo
+     * @param viewHolderMusicList
+     */
+    private void setMusicListItemInfo(final SongListInfo songListInfo, final ViewHolderMusicList viewHolderMusicList) {
+        if (songListInfo.getCoverUrl() == null) {
+            viewHolderMusicList.ivCover.setTag(R.id.tag_songlistviewholder, songListInfo.getTitle());
+            viewHolderMusicList.ivCover.setImageResource(R.drawable.default_cover);
+            viewHolderMusicList.tvMusic1.setText("");
+            viewHolderMusicList.tvMusic2.setText("");
+            viewHolderMusicList.tvMusic3.setText("");
+            OkHttpUtils.get().url(Constants.BASE_URL)
+                    .addParams(Constants.PARAM_METHOD, Constants.METHOD_GET_MUSIC_LIST)
+                    .addParams(Constants.PARAM_TYPE, songListInfo.getType())
+                    .addParams(Constants.PARAM_SIZE, "3")
+                    .build()
+                    .execute(new JsonCallBack<JsonOnlineMusicList>(JsonOnlineMusicList.class) {
+                        @Override
+                        public void onError(Call call, Exception e) {
+                        }
+
+                        @Override
+                        public void onResponse(JsonOnlineMusicList response) {
+                            if (response == null || response.getSong_list() == null) {
+                                //        Log.i("iii", "response == null ");
+                                return;
+                            }
+
+                            if (!songListInfo.getTitle().equals(viewHolderMusicList.ivCover.getTag(R.id.tag_songlistviewholder))) {
+                                return;
+                            }
+                            List<OnlineMuiscBean> jsonlist = response.getSong_list();
+                            Log.i("iii", response.getSong_list().get(0).getArtist_name() + "|" +
+                                    response.getSong_list().get(0).getTitle());
+
+                            songListInfo.setCoverUrl(response.getBillboard().getPic_s260());
+
+
+                            /**
+                             * java.lang.IndexOutOfBoundsException: Invalid index 1, size is 1
+                             */
+                            if (response.getSong_list().size() >= 1) {
+                                songListInfo.setMusic1(jsonlist.get(0).getTitle());
+                            } else {
+                                songListInfo.setMusic1("");
+                            }
+                            if (response.getSong_list().size() >= 2) {
+                                songListInfo.setMusic2(jsonlist.get(1).getTitle());
+                            } else {
+                                songListInfo.setMusic2("");
+                            }
+                            if (response.getSong_list().size() >= 3) {
+                                songListInfo.setMusic3(jsonlist.get(2).getTitle());
+                            } else {
+                                songListInfo.setMusic3("");
+                            }
+
+
+                            Glide.with(mContext).load(songListInfo.getCoverUrl())
+                                    .error(R.drawable.default_cover)
+                                    .into(viewHolderMusicList.ivCover);
+                            viewHolderMusicList.tvMusic1.setText(songListInfo.getMusic1());
+                            viewHolderMusicList.tvMusic2.setText(songListInfo.getMusic2());
+                            viewHolderMusicList.tvMusic3.setText(songListInfo.getMusic3());
+                        }
+                    });
+
+        } else {
+            viewHolderMusicList.ivCover.setTag(R.id.tag_songlistviewholder, null);
+            Glide.with(mContext).load(songListInfo.getCoverUrl())
+                    .error(R.drawable.default_cover)
+                    .into(viewHolderMusicList.ivCover);
+            viewHolderMusicList.tvMusic1.setText(songListInfo.getMusic1());
+            viewHolderMusicList.tvMusic2.setText(songListInfo.getMusic2());
+            viewHolderMusicList.tvMusic3.setText(songListInfo.getMusic3());
+        }
+    }
+
+
 }
